99773d183b6a0f0421f15081a723c568bc311184
IN007848 <Reshma@oppo2019>
2019-10-21 17:29:24 +0530

    Reference code

---
+++ app/src/main/java/com/example/kotlinexample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*  https://medium.com/@tejumoladavid_91868/how-to-implement-an-horizontal-timed-scrolling-recyclerview-in-android-e4da369532f0
+        https://blog.davidmedenjak.com/android/2017/06/24/viewpager-recyclerview.html
+        https://www.tutorialkart.com/kotlin-android-tutorial/
+        https://www.tutorialspoint.com/kotlin/kotlin_control_flow.htm
+        https://www.javatpoint.com/kotlin-android-textview-and-edittext
+        https://www.programiz.com/kotlin-programming/hello-world
+        https://www.youtube.com/watch?v=VEqhzCFmEQI&list=PLlxmoA0rQ-LwgK1JsnMsakYNACYGa1cjR*/
+    }
+}
+
